cf02675e2facde9203d59f19acfca0d8679a2f5d
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-01-23 14:42:24 -0700

    numbers notes

---
+++ notes/intergers_floats.py
@@ -0,0 +1,15 @@
+"""price = float(input("How much did it cost: $"))
+
+sales_tax = float(input("How much is the sales tax: %"))
+
+tax = price * sales_tax
+
+total = round(price + (price * sales_tax),2)
+
+print(total)"""
+
+apples = int(input("How many apples are there: "))
+
+friends = 8
+
+print("Each friend can have", apples//friends, "and I have", apples%friends, "left over")
