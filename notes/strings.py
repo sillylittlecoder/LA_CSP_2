57260af004db41c96c778bbf05bfbc7a9da9e726
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-01-27 14:42:20 -0700

    strings notes

---
+++ notes/strings.py
@@ -0,0 +1,17 @@
+#name = input("What is your name: ").strip().title()
+
+#print("Hello", name + "!")
+
+"""sentence = "the quick brown fox jumps over the lazy dog."
+print(sentence)
+word = input("Tell me a word from the sentence: ")
+new_word = input("Give me a new word: ")
+start = sentence.find(word)
+length = len(word)
+print(sentence.replace(word, new_word))
+
+#print(sentence[start:start+length])"""
+
+num_one = int(input("Tell me a number: "))
+num_two = int(input("Tell me a number: "))
+print(num_one * num_two)
