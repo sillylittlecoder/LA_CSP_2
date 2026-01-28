d4aa20ff48ac53ddfd2e5f655a413b20fe3d68df
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-01-28 14:43:00 -0700

    worked in class

---
+++ practices/silly_sentences.py
@@ -0,0 +1,15 @@
+# LA silly sentences
+person = input("Give me a person(name, celebrity, etc.): ").strip().title
+adjective = input("Give me an adjective: ").strip()
+adjective2 = input("Give me another adjective!: ").strip()
+noun = input("Give me a noun ending in -ing: ").strip()
+adjective3 = input("How about another adjective!: ").strip()
+noun2 = input("Give me a second noun: ").strip()
+adjective4 = input("Another adjective wouldn't hurt: ").strip()
+adjective5 = input("Okay give me the last adjective: ").strip()
+verb = input("Give me a present tense verb: ").strip()
+verb2 = input("Give me a second present tense verb: ").strip()
+verb3 = input("Give me a third present tense verb: ").strip()
+verb4 = input("One last verb please: ").strip()
+
+print("Yesterday,",person,"and I went to the park. On our way to the"+adjective+ " "+ "park, we saw a(n)"+" "+adjective2+" "+noun+" "+"on a bike. We also saw big"+" "+adjective3+" "+"balloons tied to a"+" "+noun2+".Once we got to the"+" "+adjective+" "+"park, the sky turned"+adjective4+".It started to"+" "+verb+" "+"and"+" "+verb2+"."+person +" and I"+" "+verb3+" all the way home. Tomorrow we will try to go to the"+" "+adjective+" park again and hope it doesn't"+" "+verb4+".")
