9d532d5376e9d32bd41868ed56045583db15e6cd
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-02-04 19:14:52 +0000

    finished fizzbuzz homework

--- practices/fizzbuzz.py
+++ practices/fizzbuzz.py
@@ -2,13 +2,14 @@
 
 start = 1
 
-while start >= 1 and start <= 50:
-    print(start)
-    start += 1
-    if start%3 == 0:
-        print(f"Fizz")
-    if start%5 == 0:
-        print(f"Buzz")
+while start <= 50:
+
     if start%3 == 0 and start%5 == 0:
         print(f"FizzBuzz")
-    
+    elif start%5 == 0:
+        print(f"Buzz")
+    elif start%3 == 0:
+        print(f"Fizz")
+    else:
+        print(start)
+    start += 1
