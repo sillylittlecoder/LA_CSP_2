753f71467e621899c344b753524fbe8fde721f5b
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-02-09 14:42:00 -0700

    worked on final project Co-authored-by: Rubber-duck1234 <Rubber-duck1234@users.noreply.github.com>

--- practices/final_project.py
+++ practices/final_project.py
@@ -1,23 +1,74 @@
-print("Instructions: To play hangman, you will be given a word you need to gues(by letter). Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win. However, if you guess too many wrong times and the person is built, you lose.")
-print("-----")
-print("|")
-print("|")
-print("|")
-print("-----")
-
+import random
 words = ["Onyx", "Xylaphone", "Glyph", "Syzygy", "Zuzwang", "House", "Dermatoglyphics", "Xebec", "Bdellium", "Grawlix",]
+random = random.choice(words)
+"""print(random)
+"""
 
-number = input("Pick a number between 0 and 9: ")
+print("Instructions: To play hangman, you will be given a word you need to guess(by letter). Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win. However, if you guess too many wrong times and the person is built, you lose.")
+guess = input("Chose a letter: ")
 
-word = (0,9)
-while number == (0,9):
-    if number == 0:
-        word = words[0]
-    elif number == 1:
-        word = words[1]
 
-print(words)
+while loss < 5
+    '''--------
+   |      |
+   |      O
+   |    \ | /
+   |     / \
+   |    -   -
+   --------'''
 
+
+if loss == 0:
+    print('''--------
+             |      |
+             |
+             |
+             |  
+             |
+             --------''')
+elif loss == 1:
+    print(
+'''--------
+   |      |
+   |      O
+   |
+   |
+   |
+   --------''')
+elif loss == 2:
+    print('''
+   --------
+   |      |
+   |      O
+   |      |
+   |
+   |
+   --------''')
+elif loss == 3:
+    print('''--------
+   |      |
+   |      O
+   |      |
+   |     / \
+   |
+   --------''')
+elif loss == 4:
+    print('''--------
+   |      |
+   |      O
+   |    \ | /
+   |     / \
+   |
+   --------''')
+elif loss == 5:
+    print('''--------
+   |      |
+   |      O
+   |    \ | /
+   |     / \
+   |    -   -
+   --------''')
+    print("Lol. U hung him.")
 """while True:
 
     guess = input("Guess a singular letter: ").strip().lower()
