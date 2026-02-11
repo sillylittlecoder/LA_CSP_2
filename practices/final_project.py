59e10e3053a41b199c03fd1ddcb90dc7e9f41578
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-02-11 14:42:36 -0700

    final project
    Co-authored-by: Rubber-duck1234 <Rubber-duck1234@users.noreply.github.com>

--- practices/final_project.py
+++ practices/final_project.py
@@ -1,51 +1,68 @@
 import random
 words = ["Onyx", "Xylaphone", "Glyph", "Syzygy", "Zuzwang", "House", "Dermatoglyphics", "Xebec", "Bdellium", "Grawlix",]
-random = random.choice(words)
-display = ""
-"""
-def guessed_letters(letters):
-    return (input(f"Guess a singular {letters}: "))
+word = random.choice(words)
+random.len()
 
-print()"""
+"""---------
+   |        |
+   |        O
+   |    \\  | /
+   |        /
+   |       / \\
+   --------
+    """
+#work on finishing it
 
 print("Instructions: To play hangman, you will be given a word you need to guess(by letter). Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win. However, if you guess too many wrong times and the person is built, you lose.")
+loss = 0
+letter = []
 
-guess = input("guess a singluar letter: ")
+while True:
+    display = ""
 
-if guess in random:    #for loop to loop over each letter in word   
+    guess = input("guess a singular letter: ")
+    guess.append(letter)
+    for letter in random:
+        if letter == guess:
+          display += letter
 
-if letter == words:   #if letter in guessed list
-    print(word)
 
-while loss <= 8:
 
+    print(display)
+
+    if letter == display:
+        print("You won!")
+        break
+
+    if guess not in random: 
+        loss += 1    
 
     '''--------
    |      |
    |      O
-   |    \ | /
+   |    \\ | /
    |     / \\
    |    -   -
    --------'''
-if loss == 0:
-    print('''--------
+    if loss == 0:
+        print('''--------
              |      |
              |
              |
              |  
              |
              --------''')
-elif loss == 1:
-    print(
-'''--------
+    elif loss == 1:
+        print(
+        '''--------
    |      |
    |      O
    |
    |
    |
    --------''')
-elif loss == 2:
-    print('''
+    elif loss == 2:
+        print('''
    --------
    |      |
    |      O
@@ -53,60 +70,56 @@ elif loss == 2:
    |
    |
    --------''')
-elif loss == 3:
-    print('''--------
+    elif loss == 3:
+        print('''--------
    |      |
    |      O
    |      |
    |     / 
    |
    --------''')
-elif loss == 4:
-    print( '''--------
+    elif loss == 4:
+        print( '''--------
    |      |
    |      O
    |      | 
    |     / \\
    |    
    --------''')
-elif loss == 5:
-    print('''--------
+    elif loss == 5:
+        print('''--------
    |      |
    |      O
-   |    \ | 
+   |    \\ | 
    |     / \\
    |
    --------''')
-elif loss == 6:
-    print('''--------
+    elif loss == 6:
+        print('''--------
    |      |
    |      O
-   |    \ | /
+   |    \\ | /
    |     / \\
    |       
    --------''')
-elif loss == 7:
-    print('''--------
+    elif loss == 7:
+        print('''--------
    |      |
    |      O
-   |    \ | /
+   |    \\ | /
    |     / \\
    |    -   
    --------''')
-elif loss == 8:
-    print( '''--------
+    elif loss == 8:
+        print('''--------
    |      |
    |      O
-   |    \ | /
+   |    \\ | /
    |     / \\
-   |    -   -
-   --------''')
-    print("Wow. Eight guesses and you still lost! You have hung hangman.")
+   |    -   -')
+          print("Wow. ight guesses and you still lost! You have hung hangman.")
+      breakngm'')
+n.")
     break
-"""while True:
 
-    guess = input("Guess a singular letter: ").strip().lower()
-    if guess == letter:
-
-"""
-# need to figure out how to check letters in word and display them
+ut how to check letters in word and display them
