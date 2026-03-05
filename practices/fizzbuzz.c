5dbe0f1bb843b45b2f43fb260d84642ea4007ddd
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-03-05 02:57:50 +0000

    homework success

--- practices/fizzbuzz.c
+++ practices/fizzbuzz.c
@@ -5,6 +5,17 @@
 
 int main(){
     int start = 1;
-    while (start%3 == 0 && start%5 == 0);
+    while(start <= 50){ 
+        if (start%3 == 0 && start%5 == 0){
+            printf("FizzBuzz\n");
+        }else if (start%5 == 0){
+            printf("Buzz\n");
+        }else if (start%3 == 0){
+            printf("Fizz\n");
+        }else{
+            printf("%d\n", start);
+        }
+        start++;
+    }
     return 0;
 }
