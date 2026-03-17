d26a88cd33fe414f72c348c4552aaadc566689d9
sillylittlecoder <lilliann.avila@ucas-edu.net>
2026-03-17 14:41:43 -0600

    worked on final project
    Co-authored-by: ilikeolives-star <ilikeolives-star@users.noreply.github.com>

--- final_project.c
+++ final_project.c
@@ -6,29 +6,11 @@
 #include <time.h>
 
 int main(){
-    int number = (rand() % 30) + 1;
-
-    while (true){
-        int guess;
-        printf("guess a singular letter: ");
-        scanf("%d", &guess);
-
-        if (guess == number){
-            printf("You WON! The number was %d.\n", number);
-        }else if (guess < number){
-            printf("guess higher\n");
-        }else{
-            printf("guess lower\n");
-        }
-    }
-
-char words[][20] = {"onyx", "xylaphone", "glyph", "syzygy", "zuzwang", "house", "dermatoglyphics", "xebec", ""}; // worked on this section
-    for(char letter in word)
-        if letter in let_list:
-            display += letter
-        else:
-            display += "_"
-    print(display)
+    srand(time(NULL));
+    int number = rand() % 10 +1;
+    int count = 0;
+    char words[][20] = {"onyx", "xylaphone", "glyph", "syzygy", "zuzwang", "house", "dermatoglyphics", "xebec", "bdellium", "grawlix",};
+    printf("%s\n", words[6]);
 
 
     printf("Instructions: To play hangman, you will be given a word you need to guess(by letter). Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win. However, if you guess too many wrong times and the person is built, you lose."); 
